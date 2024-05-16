f37ff3354c3d33e18dbd0324b587df209fac45e1
Alexandr Fonov <FonovAD@Gmail.com>
2024-05-16 18:14:57 +0300

    style rename sessionrepository

--- migrations/20240514072508_users_add_refresh_token.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users DROP COLUMN refresh_token;
-ALTER TABLE users DROP COLUMN exp_refresh_token;
--- migrations/20240514072508_users_add_refresh_token.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE users ADD COLUMN refresh_token varchar unique;
-ALTER TABLE users ADD COLUMN exp_refresh_token integer;
