f5573dc99332bf978fe88f0f83aef2f7e748e4ff
Alexandr Fonov <FonovAD@Gmail.com>
2024-06-24 18:42:49 +0300

    chore add middleware
    Added logging and server recovery after a panic

--- internal/apiserver/handlers.go
+++ internal/apiserver/handlers.go
@@ -2,7 +2,6 @@ package apiserver
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
 	"time"
 
@@ -20,26 +19,6 @@ type brokerMessage struct {
 	URL   string
 }
 
-func (s *server) HandleHello() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		HTML := `<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Document</title>
-		</head>
-		<body style="margin-left: 3vw; margin-top: 2vh;">
-			<h1>Hello</h1>
-		</body> 
-		</html>
-		`
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(HTML))
-		s.logger.Infof("%s\t%s", r.Method, r.URL)
-	}
-}
-
 func GetFingerPrint(r *http.Request) string {
 	fingerPrint := r.Header.Get("Accept-Language") + r.Header.Get("Sec-Ch-Ua-Platform") + r.Header.Get("User-Agent")
 	return fingerPrint
@@ -55,12 +34,34 @@ func (s *server) error(w http.ResponseWriter, statusCode int, message string) er
 		Status_code: statusCode,
 		Message:     message,
 	}
+	s.logger.Errorf("status code: %d, Error: %s", statusCode, message)
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		s.logger.Errorf("Error:%s", err)
+		s.logger.Errorf("status code: %d, Error: %s", http.StatusInternalServerError, err.Error())
 		return errorResponse{Status_code: http.StatusInternalServerError}
 	}
 	return resp
+}
+
+func (s *server) HandleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		HTML := `<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Document</title>
+		</head>
+		<body style="margin-left: 3vw; margin-top: 2vh;">
+			<h1>Hello</h1>
+		</body> 
+		</html>
+		`
+		// s.error(w, http.StatusMethodNotAllowed, "Only the POST method is allowed")
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(HTML))
+	}
 }
 
 func (s *server) Login() http.HandlerFunc {
@@ -76,13 +77,11 @@ func (s *server) Login() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "POST" {
 			s.error(w, http.StatusMethodNotAllowed, "Only the POST method is allowed")
-			s.logger.Warnf("%s\t%s\tError: %s", r.Method, r.URL, "MethodNotAllowed")
 			return
 		}
 		req := &request{}
 		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
 			s.error(w, http.StatusUnprocessableEntity, "Incorrect request fields")
-			s.logger.Warnf("%s\t%s\t  %d\tError: %s", r.Method, r.URL, http.StatusUnprocessableEntity, err.Error())
 			return
 		}
 
@@ -90,23 +89,19 @@ func (s *server) Login() http.HandlerFunc {
 		if err != nil {
 			if err == store.ErrRecordNotFound {
 				s.error(w, http.StatusNotFound, "There is no user with this email address")
-				s.logger.Warnf("%s\t%s\t  %d\tError: %s", r.Method, r.URL, http.StatusNotFound, err.Error()) //сделать нормалныен ошибки, чтобы их можно было сообщать пользователю
 				return
 			}
 			s.error(w, http.StatusInternalServerError, "Server error - the user could not be found")
-			s.logger.Errorf("%s\t%s\t  %d\tError: %s", r.Method, r.URL, http.StatusNotFound, err.Error()) //сделать нормалныен ошибки, чтобы их можно было сообщать пользователю
 			return
 		}
 		if !expectedU.ComparePassword(req.Password) {
 			w.WriteHeader(http.StatusUnauthorized)
-			s.logger.Infof("%s\t%s", r.Method, r.URL)
 			return
 		}
 		if !expectedU.Verified {
 			VerToken, err := token_generator.GenerateRandomString(64)
 			if err != nil {
 				s.error(w, http.StatusInternalServerError, "User registration error")
-				s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 				return
 			}
 
@@ -117,14 +112,12 @@ func (s *server) Login() http.HandlerFunc {
 
 			if err := s.store.User().UpdateVerificationToken(expectedU.Email, VerToken); err != nil {
 				s.error(w, http.StatusInternalServerError, "Failed to update verification token")
-				s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 				return
 			}
 
 			body, err := json.Marshal(message)
 			if err != nil {
 				s.error(w, http.StatusInternalServerError, "Failed to send verification message")
-				s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 				return
 			}
 
@@ -134,7 +127,6 @@ func (s *server) Login() http.HandlerFunc {
 			}
 
 			s.error(w, http.StatusUnauthorized, "User not verified")
-			s.logger.Infof("%s\t%s\t  %d\tError: %s", r.Method, r.URL, http.StatusUnauthorized, errors.New("user not verified"))
 			return
 		}
 
@@ -143,7 +135,6 @@ func (s *server) Login() http.HandlerFunc {
 		session, err := s.store.Session().CreateSession(uint32(expectedU.ID), GetFingerPrint(r))
 		if err != nil {
 			s.error(w, http.StatusInternalServerError, "Failed to create a session")
-			s.logger.Errorf("%s\t%s\t  %d\tError: %s", r.Method, r.URL, http.StatusInternalServerError, err.Error()) //сделать нормалныен ошибки, чтобы их можно было сообщать пользователю
 			return
 		}
 		resp := response{
@@ -152,10 +143,8 @@ func (s *server) Login() http.HandlerFunc {
 		}
 		if err := json.NewEncoder(w).Encode(resp); err != nil {
 			s.error(w, http.StatusUnprocessableEntity, "")
-			s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 			return
 		}
-		s.logger.Infof("%s\t%s", r.Method, r.URL)
 	}
 }
 
@@ -166,18 +155,15 @@ func (s *server) Logout() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "POST" {
 			s.error(w, http.StatusMethodNotAllowed, "Only the POST method is allowed")
-			s.logger.Warnf("%s\t%s\tError: %s", r.Method, r.URL, "MethodNotAllowed")
 			return
 		}
 		req := &request{}
 		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
 			s.error(w, http.StatusUnprocessableEntity, "Incorrect request fields")
-			s.logger.Warnf("%s\t%s\t  %d\tError: %s", r.Method, r.URL, http.StatusUnprocessableEntity, err.Error())
 			return
 		}
 		if err := s.store.Session().DeleteSession(GetFingerPrint(r), req.RefreshToken); err != nil {
 			s.error(w, http.StatusInternalServerError, "Failed to delete a session")
-			s.logger.Errorf("%s\t%s\t  %d\tError: %s", r.Method, r.URL, http.StatusInternalServerError, err.Error()) //сделать нормалныен ошибки, чтобы их можно было сообщать пользователю
 			return
 		}
 	}
@@ -195,26 +181,22 @@ func (s *server) GetAccessToken() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
 			s.error(w, http.StatusMethodNotAllowed, "Only the GET method is allowed")
-			s.logger.Warnf("%s\t%s\tError: %s", r.Method, r.URL, "MethodNotAllowed")
 			return
 		}
 		req := &request{}
 		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
 			s.error(w, http.StatusUnprocessableEntity, "Incorrect request fields")
-			s.logger.Warnf("%s\t%s\t  %d\tError: %s", r.Method, r.URL, http.StatusUnprocessableEntity, err.Error())
 			return
 		}
 		session, err := s.store.Session().VerifyRefreshToken(GetFingerPrint(r), req.RefreshToken)
 		if err != nil {
 			w.WriteHeader(http.StatusNonAuthoritativeInfo)
 			s.error(w, http.StatusUnauthorized, "The session for this user was not found")
-			s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 			return
 		}
 		user, err := s.store.User().GetUserByID(int(session.UserId))
 		if err != nil {
 			s.error(w, http.StatusInternalServerError, "")
-			s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 			return
 		}
 		payload := jwt.MapClaims{
@@ -226,7 +208,6 @@ func (s *server) GetAccessToken() http.HandlerFunc {
 		accessToken, err := token.SignedString([]byte(s.jwtSecretKey))
 		if err != nil {
 			s.error(w, http.StatusInternalServerError, "Failed to generate accessToken")
-			s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 			return
 		}
 		resp := response{
@@ -236,10 +217,8 @@ func (s *server) GetAccessToken() http.HandlerFunc {
 		}
 		if err := json.NewEncoder(w).Encode(resp); err != nil {
 			s.error(w, http.StatusUnprocessableEntity, "")
-			s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 			return
 		}
-		s.logger.Infof("%s\t%s", r.Method, r.URL)
 	}
 }
 
@@ -251,19 +230,16 @@ func (s *server) Registration() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "POST" {
 			s.error(w, http.StatusMethodNotAllowed, "Only the POST method is allowed")
-			s.logger.Warnf("%s\t%s\tError: %s", r.Method, r.URL, "MethodNotAllowed")
 			return
 		}
 		req := &request{}
 		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
 			s.error(w, http.StatusUnprocessableEntity, "Incorrect request fields")
-			s.logger.Warnf("%s\t%s\tError: %s", r.Method, r.URL, "Incorrect request fields")
 			return
 		}
 		VerToken, err := token_generator.GenerateRandomString(64)
 		if err != nil {
 			s.error(w, http.StatusInternalServerError, "User registration error")
-			s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 			return
 		}
 		u := &model.User{
@@ -273,7 +249,6 @@ func (s *server) Registration() http.HandlerFunc {
 		}
 		if err := s.store.User().Create(u); err != nil {
 			s.error(w, http.StatusUnprocessableEntity, err.Error())
-			s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 			return
 		}
 		u.Sanitize()
@@ -286,7 +261,6 @@ func (s *server) Registration() http.HandlerFunc {
 		body, err := json.Marshal(message)
 		if err != nil {
 			s.error(w, http.StatusInternalServerError, "Failed to send verification message")
-			s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 			return
 		}
 
@@ -296,11 +270,9 @@ func (s *server) Registration() http.HandlerFunc {
 		}
 		if err := json.NewEncoder(w).Encode(u); err != nil {
 			s.error(w, http.StatusUnprocessableEntity, "")
-			s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 			return
 		}
 		w.WriteHeader(http.StatusCreated)
-		s.logger.Infof("%s\t%s", r.Method, r.URL)
 	}
 }
 
@@ -308,7 +280,6 @@ func (s *server) Verification() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET" {
 			s.error(w, http.StatusMethodNotAllowed, "Only the POST method is allowed")
-			s.logger.Warnf("%s\t%s\tError: %s", r.Method, r.URL, "MethodNotAllowed")
 			return
 		}
 		token := r.PathValue("token")
@@ -317,16 +288,13 @@ func (s *server) Verification() http.HandlerFunc {
 		ok, err := s.store.User().CheckVerificationToken(email, token)
 		if err != nil {
 			s.error(w, http.StatusInternalServerError, "Server error")
-			s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 			return
 		}
 		err = s.store.User().SetVerify(email, ok)
 		if err != nil {
 			s.error(w, http.StatusInternalServerError, "Server error")
-			s.logger.Errorf("%s\t%s\tError: %s", r.Method, r.URL, err.Error())
 			return
 		}
 		w.WriteHeader(http.StatusOK)
-		s.logger.Infof("%s\t%s", r.Method, r.URL)
 	}
 }
