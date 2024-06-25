04033132aafd81f91a1ba435cdb6a1ea08d742ca
Alexandr Fonov <FonovAD@Gmail.com>
2024-06-25 14:34:02 +0300

    chore add errors.go in apiserver

---
+++ internal/apiserver/errors.go
@@ -0,0 +1,13 @@
+package apiserver
+
+import "errors"
+
+var (
+	ErrorOnlyPostMethod        = errors.New("only the POST method is allowed")
+	ErrorRequestFields         = errors.New("incorrect request fields")
+	ErrorNotFoundUserWithEmail = errors.New("there is no user with this email address")
+	ErrorServer                = errors.New("server error")
+	ErrorUserNotVerified       = errors.New("user not verified")
+	ErrorOnlyGetMethod         = errors.New("only the GET method is allowed")
+	ErrorUserUnauth            = errors.New("the session for this user was not found")
+)
