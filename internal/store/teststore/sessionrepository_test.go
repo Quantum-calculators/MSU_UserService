ff7dc186352e190a2d104b27ed3f69e410f1beab
Alexandr Fonov <FonovAD@Gmail.com>
2024-07-01 15:17:15 +0300

    chore add test for DeleteAllSession

--- internal/store/teststore/sessionrepository_test.go
+++ internal/store/teststore/sessionrepository_test.go
@@ -52,3 +52,19 @@ func TestSessionRepository_DeleteSession(t *testing.T) {
 	err2 := s.Session().DeleteSession(nil, session.Fingerprint, "invalidResreshToken")
 	assert.Error(t, err2)
 }
+
+func TestSessionRepository_DeleteAllSession(t *testing.T) {
+	s := testStore.New()
+
+	session := model.TestSession(t)
+	u := model.TestUser(t)
+	err := s.User().Create(nil, u)
+	assert.NoError(t, err)
+	session, err2 := s.Session().CreateSession(nil, u.Email, session.Fingerprint)
+	assert.NoError(t, err2)
+	_, err3 := s.Session().CreateSession(nil, u.Email, "Another fingerprint")
+	assert.NoError(t, err3)
+
+	err1 := s.Session().DeleteAllSession(nil, session.Email)
+	assert.NoError(t, err1)
+}
